0b03d91eac99becc094d90e1faaba00b428ee363
Yaman Umuroglu <yamanu@amd.com>
2024-08-20 12:22:28 +0000

    add wide (64/128-bit) counter testcase for Verilog

--- py/test.py
+++ py/test.py
@@ -60,6 +60,33 @@ def test_counting_verilog():
 
         (old_a, old_b) = (a, b)
 
+def test_counting_wide_verilog():
+    xsi = pyxsi.XSI("xsim.dir/counter_wide_verilog/xsimk.so", is_toplevel_verilog=1, tracefile="counter_wide_verilog.wdb")
+
+    (old_a, old_b) = (f"{0:064b}", f"{0:064b}")
+
+    for n in range(2**32, (2**32) + 10):
+        xsi.set_port_value("clk", "1")
+        xsi.run(HALF_PERIOD)
+        xsi.set_port_value("clk", "0")
+        xsi.run(HALF_PERIOD)
+
+        xsi.set_port_value("a", f"{(n+1) & 0xffffffffffffff:064b}")
+        xsi.set_port_value("b", f"{(n+2) & 0xffffffffffffff:064b}")
+
+        a = xsi.get_port_value("a")
+        b = xsi.get_port_value("b")
+        sum = xsi.get_port_value("sum")
+        product = xsi.get_port_value("product")
+
+        print(f"old_a {old_a} old_b {old_b} sum {sum} product {product}")
+        print(f"old_a {int(old_a, 2)} old_b {int(old_b, 2)} sum {int(sum,2)} product {int(product,2)}")
+
+        assert (int(old_a, 2) + int(old_b, 2)) % 2 ** 64 == int(sum, 2)
+        assert (int(old_a, 2) * int(old_b, 2)) % 2 ** 128 == int(product, 2)
+
+        (old_a, old_b) = (a, b)
+
 def test_random():
     xsi = pyxsi.XSI("xsim.dir/widget/xsimk.so", is_toplevel_verilog=0, tracefile="random.wdb")
 
--- rtl/counter.v
+++ rtl/counter.v
@@ -25,3 +25,29 @@ always @ (posedge clk)
      end
   end
 endmodule
+
+module counter_wide_verilog (clk, rst, a, b, sum, product);
+input rst, clk;
+input [63:0] a;
+input [63:0] b;
+output [63:0] sum;
+output [127:0] product;
+
+reg  [63:0] reg_sum;
+reg [127:0] reg_product;
+
+assign sum = reg_sum;
+assign product = reg_product;
+
+always @ (posedge clk)
+  begin
+     if (rst) begin
+       reg_sum <= 64'h0;
+       reg_product <= 128'h0;
+     end 
+     else begin
+       reg_sum <= a+b;
+       reg_product <= a*b;
+     end
+  end
+endmodule
